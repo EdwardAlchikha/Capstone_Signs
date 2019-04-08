8fbc387ff133835cdb35376a197301392a5c85ea
Edward Alchikha <edwardalchikha@gmail.com>
2019-04-07 22:22:33 -0400

    better pre-processing that zooms in on signs, and model to use with it. works with very high accuracy so long as it can find the sign's region, which it can usually do easily so long as the sign is the only very red thing in the top-right quadrant of the image

--- predict_sign.py
+++ predict_sign.py
@@ -8,13 +8,55 @@ class SignPredicter:
         self.model = load_model(model_name)
         
     def predict(self, image):
-        image = image[0:180, 460:640, :]
-        image = cv2.resize(image, (32, 32))
+        image = image[0:240, 320:640, :]
+        
+        red_mask = 1.75 * image[:, :, 2] - 0.75 * image[:, :, 1] - 0.75 * image[:, :, 0]
+        red_mask[red_mask < 120] = 0
+        red_mask[red_mask >= 120] = 255
+        red_mask = np.uint8(red_mask)
+        red_mask = cv2.dilate(red_mask, None, iterations=5)
+        
+        filling_contours = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[0]
+        filling_mask = np.zeros(image.shape[:2], np.uint8)
+        red_mask = cv2.drawContours(filling_mask, filling_contours, -1, 255, -1)
+        
+        red_mask = cv2.erode(red_mask, None, iterations=12)
+        red_mask = cv2.dilate(red_mask, None, iterations=8)
+        
+        contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        
+        rectangles = list(map(lambda contour: cv2.boundingRect(contour), contours))
+        rectangles = list(filter(lambda rect: (rect[2] > 25 and rect[3] > 25), rectangles))
+        
+        if len(rectangles) == 0:
+            return None
+        
+        rectangle_choice = min(rectangles, key=lambda rectangle: rectangle[1])
+        
+        x, y, w, h = rectangle_choice
+        
+        x, y, w, h = x, y, w, w
+        image = image[y:y+h, x:x+w, :]
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.resize(image, (32, 32))
         image = cv2.equalizeHist(image)
+        
         image = np.reshape(image, (32, 32, 1))
         image = np.asarray([image])
+        
         return self.model.predict_classes(image)
 
-sign_predicter = SignPredicter("./MODELS/model-350374-0.8641199952363968-0.9964000034332275-32.h5")
-print(sign_predicter.predict(cv2.imread("./40/image091.jpeg")))
+
+sign_predicter = SignPredicter("./MODELS/working-357983-10epochs-100steps-100batch.h5")
+prediction = sign_predicter.predict(cv2.resize(cv2.imread("./real-stop-test.jpg"), (640, 480)))
+
+if prediction == 0:
+    print("STOP")
+elif predicition == 1:
+    print("5")
+elif prediction == 2:
+    print("10")
+elif predicition == None:
+    print("No valid region for sign could be found.")  # != no sign, no sign not yet implemented
+else:
+    print("What did you do to make this happen?")
